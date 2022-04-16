e0a3bb53b93a3b02bf280c403b0b79c6b9478ee1
Ezekiel Halley <eze.halley@gmail.com>
2022-04-16 19:39:07 -0400

    added sign up page and began experimenting with routing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,16 +2,23 @@ import React from 'react'
 import Signup from './Signup'
 import { Container } from 'react-bootstrap'
 import { AuthProvider } from '../contexts/AuthContext'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Login from "./Login"
 
 function App() {
     return (
-        <AuthProvider>
-            <Container className ="d-flex align-items-center justify-content-center" style={{minHeight: "100vh"}}>
-                <div className='w-100' style={{maxWidth: "400px"}}>
-                    <Signup />
-                </div>
-            </Container>
-        </AuthProvider>
+        <Container className ="d-flex align-items-center justify-content-center" style={{minHeight: "100vh"}}>
+            <div className='w-100' style={{maxWidth: "400px"}}>
+                <BrowserRouter>
+                    <AuthProvider>
+                        <Routes>
+                            <Route path='signup' element={<Signup/>}/>
+                            <Route path='login' element={<Login/>}/>
+                        </Routes>
+                    </AuthProvider>
+                </BrowserRouter>
+            </div>
+        </Container>
     )
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import ReadDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import App from './components/App'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-ReadDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
   <React.StrictMode>
     "<App />"
   </React.StrictMode>,
-  document.getElementById('root')
+  
 )
